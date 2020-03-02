1527d212278b9af78366a6d56cf56cb89f4ee28d
Gudner <xsderrrt@gmail.com>
2020-03-02 21:18:25 +0300

    Изменены названия объектов

--- Lab1_arch/ClassRectangle.cs
+++ Lab1_arch/ClassRectangle.cs
@@ -8,7 +8,7 @@ namespace Lab1_arch
 {
     class ClassRectangle
     {
-        int n;
+        int n = ;
         const double a = 1;
         const double b = 100000;
         double h = (b - a) / n;
