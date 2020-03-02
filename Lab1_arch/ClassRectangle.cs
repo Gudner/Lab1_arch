b21d9536448be649e5d2c62f5aae5b8f231c06c3
Gudner <xsderrrt@gmail.com>
2020-03-02 21:10:45 +0300

    Некоторые изменения

--- Lab1_arch/ClassRectangle.cs
+++ Lab1_arch/ClassRectangle.cs
@@ -9,8 +9,8 @@ namespace Lab1_arch
     class ClassRectangle
     {
         int n;
-        double a = 1;
-        double b = 100000;
+        const double a = 1;
+        const double b = 100000;
         double h = (b - a) / n;
         public double R(int n)
     }
