fba3ca3d5a3c6ba536468823bb3ab98e0ab7f59c
Gudner <xsderrrt@gmail.com>
2020-03-02 21:08:49 +0300

    Добавлена форма

---
+++ Lab1_arch/ClassRectangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_arch
+{
+    class ClassRectangle
+    {
+        int n;
+        double a = 1;
+        double b = 100000;
+        double h = (b - a) / n;
+        public double R(int n)
+    }
+}
