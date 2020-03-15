49c9d2ced1b3abaa107df0d35674b4c952dc512c
Gudner <xsderrrt@gmail.com>
2020-03-15 19:58:06 +0300

    Добавлен третий Unit-тест

--- Lab1_arch/Rectangle.cs
+++ Lab1_arch/Rectangle.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lab1_arch
+﻿namespace Lab1_arch
 {
     public class Rectangle : Figure
     {
--- UnitTestRec/UnitTest1.cs
+++ UnitTestRec/UnitTest1.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Lab1_arch;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Lab1_arch;
+using System;
 
 namespace UnitTestRec
 {
@@ -25,5 +25,25 @@ namespace UnitTestRec
             double actual = x(n);
             Assert.AreEqual(exp_res, actual, 0.01);
         }
+
+        [TestMethod]
+        public void TestRec2()
+        {
+            //Arrange
+            int a = 1;
+            int b = 1000;
+            double exp_res = 4991449.82644873;
+            int n = 100000;
+            Rectangle rec1 = new Rectangle();
+            double h = (b - a) / n;
+
+            //Act
+            Func<double, double> x = y => rec1.Calculate(n, a, b);
+
+            //Assert
+            double actual = x(n);
+            Assert.AreEqual(exp_res, actual, 0.01);
+        }
     }
+
 }
